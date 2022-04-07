09da62c289fb6c99dd69846dd7e3b66383dc60c2
Dmitry Tischencko <dmitry.tishencko2016@yandex.ru>
2022-04-07 10:09:49 +0300

    Ex3

--- form.php
+++ form.php
@@ -2,45 +2,45 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Задние 2</title>
+    <title>Задние 3</title>
 </head>
 <body>
-    <h1>Задание 2</h1>
+    <h1>Задание 3</h1>
 
-    <form action="/" method="post">
+    <form method="POST" id="form">
         <label>
             Введите Ваше имя:<br />
-            <input name="field-name-1"
+            <input name="name"
                    value="ФИО" />
         </label><br />
         <label>
             Введите Ваш email:<br />
-            <input name="field-email"
+            <input name="email"
                    value="example@example.com"
                    type="email" />
         </label><br />
 
         <label>
             Введите Вашу дату рождения:<br />
-            <input name="field-date"
+            <input name="date"
                    value="2021-09-12"
                    type="date" />
         </label><br />
         Укажите Ваш пол:<br />
         <label>
             <input type="radio" checked="checked"
-                   name="radio-group-1" value="Значение1" />
+                   name="gender" value="Значение1" />
             Муж
         </label>
         <label>
             <input type="radio"
-                   name="radio-group-1" value="Значение2" />
+                   name="gender" value="Значение2" />
             Жен
         </label><br />
         <label>
             Укажите Вашу сверх способность из списка:
             <br />
-            <select name="field-name-4[]"
+            <select name="superpower[]"
                     multiple="multiple">
                 <option value="Значение1">Бессмертие</option>
                 <option value="Значение2">Левитация</option>
@@ -50,50 +50,34 @@
         Укажите количество Ваших конечностей:<br />
         <label>
             <input type="radio" checked="checked"
-                   name="radio-group-2" value="Значение1" />
+                   name="limb" value="Значение1" />
             1-2
         </label>
         <label>
             <input type="radio"
-                   name="radio-group-2" value="Значение2" />
+                   name="limb" value="Значение2" />
             3-4
         </label>
         <label>
             <input type="radio"
-                   name="radio-group-2" value="Значение3" />
+                   name="limb" value="Значение3" />
             Более 4
         </label><br />
         <label>
             Напишите Вашу биографию:<br />
-            <textarea name="field-name-2">....</textarea>
+            <textarea name="bio">....</textarea>
         </label><br />
         С контрактом ознакомлен(а):<br />
         <label>
             <input type="checkbox"
-                   name="check-1" />
+                   name="check" />
         </label><br />
         <input type="submit" value="Отправить" />
     </form>
 
-    <p>1) Отправляем запрос 1.0</p>
-    <img src="images/http1_0.png" alt="1.0">
+    <p>1) Создание таблиц MySQL</p>
+    <img src="images/SQL.png" alt="1.0">
     <hr>
-    <p>2) Отправляем запрос 1.1</p>
-    <img src="images/http1_1.png" alt="1.1">
-    <hr>
-    <p>3) Определить размер файла file.tar.gz, не скачивая его</p>
-    <img src="images/httpHEAD.png" alt="head">
-    <hr>
-    <p>4) Определить медиатип ресурса /image.png</p>
-    <img src="images/httpMedia.png" alt="media">
-    <p>5) Отправить комментарий на сервер по адресу /index.php</p>
-    <img src="images/httpPHP.png" alt="php">
-    <hr>
-    <p>6) Получить первые 100 байт файла /file.tar.gz</p>
-    <img src="images/httpRange.png" alt="range">
-    <hr>
-    <p>7) Определить кодировку ресурса /index.php</p>
-    <img src="images/httpCoding.png" alt="coding">
-    <hr>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -19,92 +19,77 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 // Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
 
 // Проверяем ошибки.
-$errors=FALSE;
-if(empty($_POST['name'])){
+$test = false;
+try {
 
-    $errors=TRUE;
-}
-if(empty($_POST['email'])){
-
-    $errors=TRUE;
-}
-
-if (empty($_POST['date'])) {
-
-    $errors = TRUE;
-}
-
-if(empty($_POST['gender'])){
-
-    $errors=TRUE;
-}
-
-switch($_POST['gender']) {
-    case 'woman': {
-        $gender='woman';
-        break;
+    $errors = FALSE;
+    if (empty($_POST['name'])) {
+        print('Укажите имя<br/>');
+        $errors = TRUE;
     }
-    case 'man':{
-        $gender='man';
-        break;
-    }
-};
 
-if (empty($_POST['body'])) {
-    $errors = TRUE;
-}
+    $val_em = $_POST['email'];
+    if (empty($_POST['email']) && !(preg_match("/[0-9a-z]+@[a-z]/", $val_em))) {
+        print('Правильно укажите почту<br/>');
+        $errors = TRUE;
+    }
 
-switch($_POST['body']) {
-    case 'one': {
-        $body='one';
-        break;
+    if (empty($_POST['date'])) {
+        print('Укажите дату<br/>');
+        $errors = TRUE;
     }
-    case 'two':{
-        $body='two';
-        break;
-    }
-    case 'three':{
-        $body='three';
-        break;
-    }
-    case 'four':{
-        $body='four';
-        break;
-    }
-    case 'five':{
-        $body='five';
-        break;
-    }
-};
 
-if (empty($_POST['force'])) {
+    if (empty($_POST['bio'])) {
+        print('Заполните биографию<br/>');
+        $errors = TRUE;
+    }
+    if (empty($_POST['check'])) {
+        print('Согласитесь с условиями<br/>');
+        $errors = TRUE;
+    }
 
-    $errors = TRUE;
-}
+
+    if ($errors) {
+        // При наличии ошибок завершаем работу скрипта.
+        exit();
+    }
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $date = $_POST['date'];
+    $gender = $_POST['gender'];
+    $limbs = $_POST['limb'];
+    $bio = $_POST['bio'];
+    $check = $_POST['check'];
+    $sup = implode(",", $_POST['superpower']);
+
+////Представляет собой соединение между PHP и сервером базы данных.
+//$conn = new PDO("mysql:host=localhost;dbname=u47498", 'u47498', '7927782', array(PDO::ATTR_PERSISTENT => true));
+//
+////Подготавливает инструкцию к выполнению и возвращает объект инструкции
+//$user = $conn->prepare("INSERT INTO f SET name = ?, email = ?, date = ?, gender = ?, limb = ?, bio = ?, check = ?");
+//
+////Запускает подготовленный запрос на выполнение
+//$user -> execute([$_POST['name'], $_POST['email'], date('Y-m-d', strtotime($_POST['date'])), $_POST['gender'], $_POST['limb'], $_POST['bio'], $_POST['check']]);
+//$id_user = $conn->lastInsertId();
+//
+//$user1 = $conn->prepare("INSERT INTO s SET id = ?, super_name = ?");
+//$user1 -> execute([$id_user, $sup]);
+//$result = true;
+//  user и user1 - это "дескриптор состояния".
+
 // $power1=in_array('1',$_POST['force']) ? '1' : '0';//Проверяет, присутствует ли в массиве значение (что в чем)
 // $power2=in_array('2',$_POST['force']) ? '1' : '0';
 // $power3=in_array('3',$_POST['force']) ? '1' : '0';
 
-if (empty($_POST['bio'])){
-
-    $errors= TRUE;
-}
-
-// *************
-// Тут необходимо проверить правильность заполнения всех остальных полей.
-// *************
-
-if ($errors) {
-  // При наличии ошибок завершаем работу скрипта.
-  exit();
-}
 
 // Сохранение в базу данных.
 
-$user = 'db';
-$pass = '123';
-$db = new PDO('mysql:host=localhost;dbname=test', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
-
+    $user = 'u47498';
+    $pass = '7927782';
+    $db = new PDO('mysql:host=localhost;dbname=u47498', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+    $stmt = $db->prepare("INSERT INTO f SET name = ?, email = ?, date = ?, sex = ?, limbs = ?, bio = ?");
+    $stmt->execute([$_POST['name'], $_POST['email'], $_POST['date'], $_POST['gender'], $_POST['limb'], $_POST['bio']]);
 // Подготовленный запрос. Не именованные метки.
 // try {
 //   $stmt = $db->prepare("INSERT INTO application (name) SET name = ?");
@@ -116,20 +101,39 @@ $db = new PDO('mysql:host=localhost;dbname=test', $user, $pass, array(PDO::ATTR_
 // }
 
 //  stmt - это "дескриптор состояния".
- 
+
 //  Именованные метки.
 //$stmt = $db->prepare("INSERT INTO test (label,color) VALUES (:label,:color)");
 //$stmt -> execute(array('label'=>'perfect', 'color'=>'green'));
- 
+
 //Еще вариант
-$stmt = $db->prepare("INSERT INTO users VALUES (:firstname, :lastname, :email)");
-$stmt->bindParam(':firstname', $firstname);
-$stmt->bindParam(':lastname', $lastname);
-$stmt->bindParam(':email', $email);
-$firstname = "John";
-$lastname = "Smith";
-$email = "john@test.com";
-$stmt->execute();
+//$stmt = $db->prepare("INSERT INTO f VALUES (:name, :email, :date, :sex, :limbs, :bio)");
+//$stmt->bindParam(':name', $name);
+//$stmt->bindParam(':email', $email);
+//$stmt->bindParam(':date', $date);
+//$stmt->bindParam(':sex', $gender);
+//$stmt->bindParam(':limbs', $limbs);
+//$stmt->bindParam(':bio', $bio);
+//
+//$stmt->execute();
+
+    $id_u = $db->lastInsertId();
+    $stmts = $db->prepare("INSERT INTO s SET id = ?, sp = ?");
+    $stmts->execute([$id_u, $sup]);
+    $test = true;
+} catch (PDOException $ex) {
+    print('Error : ' . $ex->getMessage());
+    exit();
+}
+
+if ($test) {
+    echo "Success!";
+}
+//$stmts = $db->prepare("INSERT INTO s VALUES (:id, :sp)");
+//$stmts->bindParam(':id', $id_uu);
+//$stmts->bindParam(':sp', $sup);
+////Объединяет элементы массива в строку
+//$stmts->execute();
 
 // Делаем перенаправление.
 // Если запись не сохраняется, но ошибок не видно, то можно закомментировать эту строку чтобы увидеть ошибку.
