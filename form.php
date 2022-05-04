c0830a30be60362a5054040027e3307d60b10984
Dmitry Tischencko <90377536+dtischencko@users.noreply.github.com>
2022-05-04 14:23:57 +0300

    Add files via upload

--- form.php
+++ form.php
@@ -2,39 +2,67 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Задние 3</title>
+    <title>Задние 4</title>
+    <style>
+        .error {
+            border: 2px solid red;
+        }
+    </style>
 </head>
 <body>
-    <h1>Задание 3</h1>
-
+    <h1>Задание 4</h1>
+    <div class="main">
+        <?php
+        if ($errorOutput) {
+            print('<section id="messages">');
+            if ($hasErrors)
+                print('<h2>Ошибка</h2>');
+            else
+                print('<h2>Сообщения</h2>');
+            print($errorOutput);
+//            print('</section>');
+        }
+        ?>
+        <br />
     <form method="POST" id="form">
         <label>
             Введите Ваше имя:<br />
             <input name="name"
-                   value="ФИО" />
+                <?php if (!empty($errors['name'])) {print 'class="error"';} ?>
+                   value="<?php print $values['name']; ?>"
+            />
         </label><br />
         <label>
             Введите Ваш email:<br />
             <input name="email"
-                   value="example@example.com"
-                   type="email" />
+            <?php if (!empty($errors['email'])) {print 'class="error"';} ?>
+            value="<?php print $values['email']; ?>"
+            type="email"
+            />
         </label><br />
 
         <label>
             Введите Вашу дату рождения:<br />
             <input name="date"
-                   value="2021-09-12"
-                   type="date" />
+                <?php if (!empty($errors['date'])) {print 'class="error"';} ?>
+                   value="<?php print $values['date']; ?>"
+                   type="date"
+            />
         </label><br />
         Укажите Ваш пол:<br />
         <label>
-            <input type="radio" checked="checked"
-                   name="gender" value="Значение1" />
+            <input type="radio"
+                   name="gender"
+                   value="M" <?php if ($values['gender'] == 'M') {print 'checked';} ?>
+                   checked
+            />
             Муж
         </label>
         <label>
             <input type="radio"
-                   name="gender" value="Значение2" />
+                   name="gender"
+                   value="F" <?php if ($values['gender'] == 'F') {print 'checked';} ?>
+            />
             Жен
         </label><br />
         <label>
@@ -42,35 +70,43 @@
             <br />
             <select name="superpower[]"
                     multiple="multiple">
-                <option value="Значение1">Бессмертие</option>
-                <option value="Значение2">Левитация</option>
-                <option value="Значение3">Прохождение сквозь стены</option>
+                <option value="0" <?php if ($values['superpower']['0']) {print 'selected';} ?>>Бессмертие</option>
+                <option value="1" <?php if ($values['superpower']['1']) {print 'selected';} ?>>Левитация</option>
+                <option value="2" <?php if ($values['superpower']['2']) {print 'selected';} ?>>Прохождение сквозь стены</option>
             </select>
         </label><br />
         Укажите количество Ваших конечностей:<br />
         <label>
-            <input type="radio" checked="checked"
-                   name="limb" value="Значение1" />
+            <input type="radio"
+                   name="limb"
+                <?php if ($values['limb'] == '12') {print 'checked';} ?>
+                   checked
+            />
             1-2
         </label>
         <label>
             <input type="radio"
-                   name="limb" value="Значение2" />
+                   name="limb"
+                <?php if ($values['limb'] == '34') {print 'checked';} ?>
+            />
             3-4
         </label>
         <label>
             <input type="radio"
-                   name="limb" value="Значение3" />
+                   name="limb"
+                <?php if ($values['limb'] == '4') {print 'checked';} ?>
+            />
             Более 4
         </label><br />
         <label>
             Напишите Вашу биографию:<br />
-            <textarea name="bio">....</textarea>
+            <textarea name="bio"><?php print $values['bio']; ?></textarea>
         </label><br />
         С контрактом ознакомлен(а):<br />
-        <label>
+        <label <?php if (array_key_exists('check', $errors)) {print 'class="error"';} ?>>
             <input type="checkbox"
-                   name="check" />
+                   name="check"
+                <?php if ($values['check']) {print 'checked';} ?>/>
         </label><br />
         <input type="submit" value="Отправить" />
     </form>
--- index.php
+++ index.php
@@ -3,13 +3,45 @@
 // файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
 
+$errorOutput = '';
+// Складываем признак ошибок в массив.
+$errors = array();
+$hasErrors = FALSE;
+
+$defaultValues = [
+    'name' => '',
+    'email' => '',
+    'date' => '',
+    'gender' => 'O',
+    'limb' => '1',
+    'bio' => '',
+    'check' => ''
+];
+// Складываем предыдущие значения полей в массив, если есть.
+$values = array();
+foreach (['name', 'email', 'date', 'gender', 'limb', 'bio', 'check'] as $key) {
+    $values[$key] = !array_key_exists($key . '_value', $_COOKIE) ? $defaultValues[$key] : $_COOKIE[$key . '_value'];
+}
+foreach (['name', 'email', 'date'] as $key) {
+    $errors[$key] = empty($_COOKIE[$key . '_error']) ? '' : $_COOKIE[$key . '_error'];
+    if ($errors[$key] != '')
+        $hasErrors = TRUE;
+}
+//массив суперспособностей
+$values['superpower'] = array();
+$values['superpower']['0'] = empty($_COOKIE['superpower_0_value']) ? '' : $_COOKIE['superpower_0_value'];
+$values['superpower']['1'] = empty($_COOKIE['superpower_1_value']) ? '' : $_COOKIE['superpower_1_value'];
+$values['superpower']['2'] = empty($_COOKIE['superpower_2_value']) ? '' : $_COOKIE['superpower_2_value'];
+
+
+
 // В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
 // и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
   if (!empty($_GET['save'])) {
     // Если есть параметр save, то выводим сообщение пользователю.
-    print('Спасибо, результаты сохранены.');
+      $errorOutput = 'Спасибо, результаты сохранены.<br/>';
   }
   // Включаем содержимое файла form.php.
   include('form.php');
@@ -18,50 +50,142 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 }
 // Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
 
+$trimmedPost = []; // Выгружаем сюда данные с запроса
+foreach ($_POST as $key => $value)
+    if (is_string($value))
+        $trimmedPost[$key] = trim($value);
+    else
+        $trimmedPost[$key] = $value;
+
+if (empty($trimmedPost['name'])) {
+    $errorOutput .= 'Заполните имя.<br/>';
+    $errors['name'] = TRUE;
+    setcookie('name_error', 'true');
+    $hasErrors = TRUE;
+} else {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('name_error', '', 10000);
+    $errors['name'] = '';
+}
+// Выдаем куку на день с флажком об ошибке в поле.
+setcookie('name_value', $trimmedPost['name'], time() + 30 * 24 * 60 * 60);
+$values['name'] = $trimmedPost['name'];
+
+if (!preg_match('/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/', $trimmedPost['email'])) {
+    $errorOutput .= 'Заполните email.<br/>';
+    $errors['email'] = TRUE;
+    setcookie('email_error', 'true');
+    $hasErrors = TRUE;
+} else {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('email_error', '', 10000);
+    $errors['email'] = '';
+}
+// Выдаем куку на день с флажком об ошибке в поле.
+setcookie('email_value', $trimmedPost['email'], time() + 30 * 24 * 60 * 60);
+$values['email'] = $trimmedPost['email'];
+
+if (empty($trimmedPost['date'])) {
+    $errorOutput .= 'Заполните дату рождения.<br/>';
+    $errors['date'] = TRUE;
+    setcookie('date_error', 'true');
+    $hasErrors = TRUE;
+} else {
+    setcookie('date_error', '', 10000);
+    $errors['date'] = '';
+}
+setcookie('date_value', $trimmedPost['date'], time() + 30 * 24 * 60 * 60);
+$values['date'] = $trimmedPost['date'];
+
+if (empty($trimmedPost['gender'])) {
+    $errorOutput .= 'Заполните пол.<br/>';
+    $errors['gender'] = TRUE;
+    $hasErrors = TRUE;
+}
+setcookie('gender_value', $trimmedPost['gender'], time() + 30 * 24 * 60 * 60);
+$values['gender'] = $trimmedPost['gender'];
+
+if (empty($trimmedPost['limb'])) {
+    $errorOutput .= 'Заполните количество конечностей.<br/>';
+    $errors['limb'] = TRUE;
+    $hasErrors = TRUE;
+}
+setcookie('limb_value', $trimmedPost['limb'], time() + 30 * 24 * 60 * 60);
+$values['limb'] = $trimmedPost['limb'];
+
+foreach (['0', '1', '2'] as $value) {
+    setcookie('superpower_' . $value . '_value', '', 10000);
+    $values['superpower'][$value] = FALSE;
+}
+if (array_key_exists('superpower', $trimmedPost)) {
+    foreach ($trimmedPost['superpower'] as $value) {
+        if (!preg_match('/[0-2]/', $value)) {
+            $errorOutput .= 'Неверные суперспособности.<br/>';
+            $errors['superpower'] = TRUE;
+            $hasErrors = TRUE;
+        }
+        setcookie('superpower_' . $value . '_value', 'true', time() + 30 * 24 * 60 * 60);
+        $values['superpower'][$value] = TRUE;
+    }
+}
+setcookie('bio_value', $trimmedPost['bio'], time() + 30 * 24 * 60 * 60);
+$values['bio'] = $trimmedPost['bio'];
+if (!isset($trimmedPost['check'])) {
+    $errorOutput .= 'Вы не ознакомились с контрактом.<br/>';
+    $errors['check'] = TRUE;
+    $hasErrors = TRUE;
+}
+// При наличии ошибок перезагружаем страницу и завершаем работу скрипта.
+if ($hasErrors) {
+
+    include('form.php');
+    exit();
+}
+
 // Проверяем ошибки.
 $test = false;
 try {
 
-    $errors = FALSE;
-    if (empty($_POST['name'])) {
-        print('Укажите имя<br/>');
-        $errors = TRUE;
-    }
+//    $errors = FALSE;
+//    if (empty($_POST['name'])) {
+//        print('Укажите имя<br/>');
+//        $errors = TRUE;
+//    }
+//
+//    $val_em = $_POST['email'];
+//    if (empty($_POST['email']) && !(preg_match("/[0-9a-z]+@[a-z]/", $val_em))) {
+//        print('Правильно укажите почту<br/>');
+//        $errors = TRUE;
+//    }
+//
+//    if (empty($_POST['date'])) {
+//        print('Укажите дату<br/>');
+//        $errors = TRUE;
+//    }
+//
+//    if (empty($_POST['bio'])) {
+//        print('Заполните биографию<br/>');
+//        $errors = TRUE;
+//    }
+//    if (empty($_POST['check'])) {
+//        print('Согласитесь с условиями<br/>');
+//        $errors = TRUE;
+//    }
+//
+//
+//    if ($errors) {
+//        // При наличии ошибок завершаем работу скрипта.
+//        exit();
+//    }
 
-    $val_em = $_POST['email'];
-    if (empty($_POST['email']) && !(preg_match("/[0-9a-z]+@[a-z]/", $val_em))) {
-        print('Правильно укажите почту<br/>');
-        $errors = TRUE;
-    }
-
-    if (empty($_POST['date'])) {
-        print('Укажите дату<br/>');
-        $errors = TRUE;
-    }
-
-    if (empty($_POST['bio'])) {
-        print('Заполните биографию<br/>');
-        $errors = TRUE;
-    }
-    if (empty($_POST['check'])) {
-        print('Согласитесь с условиями<br/>');
-        $errors = TRUE;
-    }
-
-
-    if ($errors) {
-        // При наличии ошибок завершаем работу скрипта.
-        exit();
-    }
-
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $date = $_POST['date'];
-    $gender = $_POST['gender'];
-    $limbs = $_POST['limb'];
-    $bio = $_POST['bio'];
-    $check = $_POST['check'];
-    $sup = implode(",", $_POST['superpower']);
+    //    $name = $_POST['name'];
+    //    $email = $_POST['email'];
+    //    $date = $_POST['date'];
+    //    $gender = $_POST['gender'];
+    //    $limbs = $_POST['limb'];
+    //    $bio = $_POST['bio'];
+    //    $check = $_POST['check'];
+    //    $sup = implode(",", $_POST['superpower']);
 
 ////Представляет собой соединение между PHP и сервером базы данных.
 //$conn = new PDO("mysql:host=localhost;dbname=u47498", 'u47498', '7927782', array(PDO::ATTR_PERSISTENT => true));
@@ -88,8 +212,11 @@ try {
     $user = 'u47498';
     $pass = '7927782';
     $db = new PDO('mysql:host=localhost;dbname=u47498', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+    $db->beginTransaction();
     $stmt = $db->prepare("INSERT INTO f SET name = ?, email = ?, date = ?, sex = ?, limbs = ?, bio = ?");
-    $stmt->execute([$_POST['name'], $_POST['email'], $_POST['date'], $_POST['gender'], $_POST['limb'], $_POST['bio']]);
+//    $stmt->execute([$_POST['name'], $_POST['email'], $_POST['date'], $_POST['gender'], $_POST['limb'], $_POST['bio']]);
+    $stmt -> execute([$trimmedPost['name'], $trimmedPost['email'], $trimmedPost['date'],
+        $trimmedPost['gender'], $trimmedPost['limb'], $trimmedPost['bio']]);
 // Подготовленный запрос. Не именованные метки.
 // try {
 //   $stmt = $db->prepare("INSERT INTO application (name) SET name = ?");
@@ -119,9 +246,13 @@ try {
 
     $id_u = $db->lastInsertId();
     $stmts = $db->prepare("INSERT INTO s SET id = ?, sp = ?");
-    $stmts->execute([$id_u, $sup]);
+//    $stmts->execute([$id_u, $sup]);
+    foreach ($trimmedPost['superpower'] as $s)
+        $stmts -> execute([$id_u, $s]);
+    $db->commit();
     $test = true;
 } catch (PDOException $ex) {
+    $db->rollBack();
     print('Error : ' . $ex->getMessage());
     exit();
 }
